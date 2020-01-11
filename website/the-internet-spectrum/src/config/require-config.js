3e9a883fa57889ba2e3ab6767c93c0dbf6c541bc
Loris Null Pointer <llgreenlight@gmail.com>
2020-01-11 16:20:22 +0100

    config for iis

--- website/the-internet-spectrum/src/config/require-config.js
+++ website/the-internet-spectrum/src/config/require-config.js
@@ -12,7 +12,7 @@ var _config = {
 (function() {
 
 
-    var _mashupPath = '/extensions' + _mashupName + '';
+    var _mashupPath = indow.location.hostname.indexOf('localhost')>=0?'/extensions':'' + _mashupName + '';
 
     require.config({
         baseUrl: (_config.isSecure ? "https://" : "http://") + _config.host + (_config.port ? ":" + _config.port : "") + _config.prefix + "resources",
